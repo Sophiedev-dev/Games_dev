e8f1b74bd57926c9018e3b718f852f7727213537
Rux-Lsr <you@example.com>
2024-05-11 11:57:41 +0100

    rendre plus beau et modulable

--- src/App.js
+++ src/App.js
@@ -1,41 +1,76 @@
-
+import React, { useState } from 'react';
 import './App.css'
-function App(){
-    return(
-        <div class="global" className='container' style={{backgroundColor:'aliceBlue'  , border: '1px'}}>
-            <div className='container-fluid' style={{marginTop:'40px'}}>
-                <div className='container' style={{float:'left',width:'35%' ,backgroundColor:'red'}}>komol danie</div>
-                <div className='container' style={{float:'right',width:'30%' ,backgroundColor:'red' }}>score : 5 pts</div>
-                <div className='container' style={{float:'right',width:'35%' ,backgroundColor:'red' }}>level : easy</div>
+import { Niveau } from './Niveau';
+import {calculerTentatives} from "./level";
+
+const GuessMyNumberGame = () => {
+
+    const [low] = useState(1);
+    const [high] = useState(100);
+    const [guess, setGuess] = useState(numberToGuess());
+    const [attempts, setAttempts] = useState(calculerTentatives(low, high, Niveau.EASY));
+    const [msg, setMsg] = useState("Guess the number ... ");
+    const [show, setShow] = useState("?")
+    const [bestScore, setBestScore] = useState(0);
+    const [userName] = useState("Rux-Lsr");
+
+    const handleResponse = () => {
+        let userGuessedNumber = document.getElementById("guessedNumber").value;
+      if (attempts === 0) {
+        setMsg("No more attemps left");
+        setShow(guess)
+      }else{
+        if(guess == userGuessedNumber){
+            setMsg("You guessed the number");
+            setShow(guess);
+            attempts == calculerTentatives(low, high, Niveau.EASY) ?
+            setBestScore(calculerTentatives(low, high, Niveau.EASY)-attempts+1):
+            setBestScore(calculerTentatives(low, high, Niveau.EASY)-attempts);
+
+        }else if(userGuessedNumber>guess){
+            setMsg(userGuessedNumber+" is Higher than the number to guess");
+            setAttempts(attempts-1);
+        }else{
+            setMsg(userGuessedNumber+" is Lower than the number to guess.");
+            setAttempts(attempts-1);
+        }
+
+      }
+    };
+
+    const reGame =()=>{
+      setGuess(numberToGuess)
+      setShow("?")
+      setAttempts(calculerTentatives(low, high, Niveau.EASY))
+    }
+
+    function numberToGuess(){
+        return Math.floor(Math.random() * (high-low)+1)
+    }
+    return (
+        <div className="game-container">
+            <header>
+                <button className="play-again-btn" onClick={reGame}>Play Again</button>
+                <p>Player : {userName}</p>
+                <p>Between {low} and {high}</p>
+            </header>
+            <h1>Guess My number Game</h1>
+            <div className="mystery-number">
+                <p>{show}</p>
             </div>
-
-            <br></br>
-
-            <div className='container-fluid' style={{marginTop:'25px'}} >
-                <center><h1>Guess My NUmber Game</h1></center>
+            <div className="guess-input">
+                <p>{msg}</p>
+                <div className='but'>
+                    <input type="number" placeholder='Entrez un nombre' id='guessedNumber'></input>
+                  <button onClick={handleResponse} >Guess</button>
+                </div>
             </div>
-            
-            <center>
-                <div style={{backgroundColor:'red' , width:'15%', borderRadius:'6%'}}><h1>?</h1></div>
-                <h5>start guessing ....</h5>
-            </center>
-
-
-            <div className='container' >
-                <input type='dropDownList' style={{float:'left'}}></input>
-                <p style={{float:'right'}}>Hight score : 100</p>
-
-            </div>
-
-            <center><button className='btn btn-success' type='button' style={{backgroundColor:'red' , border: '1px' , marginBottom:'15px'}}>Check now</button></center>
+            <footer>
+                <p>Attemps: <span>{attempts}</span></p>
+                <p>Best Score: <span>{bestScore}</span></p>
+            </footer>
         </div>
-    
-    
     );
-}
+};
 
-
-
-
-
-export default App;
+export default GuessMyNumberGame;
