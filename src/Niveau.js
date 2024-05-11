b3aa29300e7db9b78d83d929647302f4dcdb97e6
Rux-Lsr <you@example.com>
2024-05-11 06:53:22 +0100

    Enumeration pour creer les Niveaux

---
+++ src/Niveau.js
@@ -0,0 +1,7 @@
+//Implementations des niveaux du jeux dans une
+//ENUMERATION pour lister les niveau de l'application
+export const Niveau = {
+    EASY: 'facile',
+    HARD: 'difficile',
+    VERY_HARD: 'tres_difficile'
+};
